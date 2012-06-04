7fcbcd1e1cbab24913ab82f4577481de0fff7714
patrick <patrick_dj@163.com>
2012-06-04 16:25:51 +0800

    add memory leak detect.

--- lib/include/mz/libs/mz_memory.h
+++ lib/include/mz/libs/mz_memory.h
@@ -1,13 +1,20 @@
 #ifndef __MZ_MEMORY_H__
 #define __MZ_MEMORY_H__
 
+#include <mz/libs/mz_defs.h>
 #include <malloc.h>
 #include <string.h>
 
-#define mz_memcpy(dst, src, size) memcpy(dst, src, size)
-#define mz_malloc(size) calloc(1, size)
-#define mz_realloc(ptr, size) realloc(ptr, size)
-#define mz_free(obj) free(obj)
-#define mz_memset(dst, v, sz) memset(dst, v, sz)
+MZ_API void* mz_malloc_(unsigned int size, const char * file, unsigned int line);
+MZ_API void* mz_calloc_(int element, unsigned int size, const char * file, unsigned int line);
+MZ_API void* mz_realloc_(void *ptr, unsigned int size, const char * file, unsigned int line);
+MZ_API void  mz_free(void *ptr);
+MZ_API void  mz_print_memory_log();
+
+#define mz_memcpy(dst, src, size)   memcpy(dst, src, size)
+#define mz_memset(dst, v, sz)       memset(dst, v, sz)
+#define mz_malloc(size)             mz_calloc_(1, size, __FILE__, __LINE__)
+#define mz_calloc(element, size)    mz_calloc_(element, size, __FILE__, __LINE__)
+#define mz_realloc(ptr, size)       mz_realloc_(ptr, size, __FILE__, __LINE__)
 
 #endif
