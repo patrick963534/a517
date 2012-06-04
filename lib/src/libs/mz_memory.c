7db87bef5491c526cab40be953b073f3e036e1e5
patrick <patrick_dj@163.com>
2012-06-04 16:26:28 +0800

    add memory leak detect.

---
+++ lib/src/libs/mz_memory.c
@@ -0,0 +1,31 @@
+#include <mz/libs/mz_memory.h>
+#include <mz/libs/mz_assert.h>
+#include <mz/libs/mz_log.h>
+#include <mz/libs/mz_defs.h>
+#include "mz_memory_leak_detect.h"
+
+MZ_API void mz_print_memory_log()
+{
+    report_mem_leak();
+}
+
+MZ_API void* mz_malloc_(unsigned int size, const char * file, unsigned int line)
+{
+    xcalloc(1, size, file, line);
+}
+
+MZ_API void* mz_calloc_(int element, unsigned int size, const char * file, unsigned int line)
+{
+    xcalloc(element, size, file, line);
+}
+
+MZ_API void* mz_realloc_(void *ptr, unsigned int size, const char * file, unsigned int line)
+{
+    logI("NOT implement mz_realloc_ yet!!!");
+    mz_assert(0);
+}
+
+MZ_API void mz_free(void *ptr)
+{
+    xfree(ptr);
+}
