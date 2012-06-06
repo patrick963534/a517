bbcde5beedd6f0e840e9bbad518ea3f72a42058d
patrick <patrick_dj@163.com>
2012-06-06 14:00:01 +0800

    rename mz_list.h to mz_general_list.h"

--- lib/src/mz_list.c
+++ lib/src/mz_list.c
@@ -1,17 +1,17 @@
-#include <mz/mz_list.h>
+#include <mz/mz_general_list.h>
 #include <mz/libs/mz_memory.h>
 #include <mz/libs/mz_defs.h>
 
-MZ_API void mz_list_init(mz_list_t *me)
+MZ_API void mz_general_list_init(mz_general_list_t *me)
 {
     me->next = me;
     me->prev = me;
 }
 
-MZ_API void mz_list_add(mz_list_t *new, mz_list_t *head)
+MZ_API void mz_general_list_add(mz_general_list_t *new, mz_general_list_t *head)
 {
-    mz_list_t *prev = head;
-    mz_list_t *next = head->next;
+    mz_general_list_t *prev = head;
+    mz_general_list_t *next = head->next;
 
     next->prev = new;
     new->next = next;
@@ -19,19 +19,19 @@ MZ_API void mz_list_add(mz_list_t *new, mz_list_t *head)
     prev->next = new;
 }
 
-MZ_API void mz_list_remove(mz_list_t *entry)
+MZ_API void mz_general_list_remove(mz_general_list_t *entry)
 {
-    mz_list_t *prev = entry->prev;
-    mz_list_t *next = entry->next;
+    mz_general_list_t *prev = entry->prev;
+    mz_general_list_t *next = entry->next;
 
     next->prev = prev;
     prev->next = next;
 
-    entry->next = (mz_list_t *) 0;
-    entry->prev = (mz_list_t *) 0;
+    entry->next = (mz_general_list_t *) 0;
+    entry->prev = (mz_general_list_t *) 0;
 }
 
-MZ_API void mz_list_replace(mz_list_t *old, mz_list_t *new)
+MZ_API void mz_general_list_replace(mz_general_list_t *old, mz_general_list_t *new)
 {
     new->next = old->next;
     new->next->prev = new;
@@ -39,7 +39,7 @@ MZ_API void mz_list_replace(mz_list_t *old, mz_list_t *new)
     new->prev->next = new;
 }
 
-MZ_API void mz_list_delete(mz_list_t *me)
+MZ_API void mz_general_list_delete(mz_general_list_t *me)
 {
     mz_free(me);
 }
--- unit_test/src/main.c
+++ unit_test/src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <mz/mz_cunit.h>
 #include <mz/mz_libs.h>
-#include <mz/mz_list.h>
+#include <mz/mz_general_list.h>
 #include "test_cunit.h"
 
 typedef void (*test_func_t)();
@@ -9,7 +9,7 @@ typedef void (*test_func_t)();
 typedef struct test_unit_t
 {
     test_func_t test_func;
-    mz_list_t   element;
+    mz_general_list_t   element;
 } test_unit_t;
 
 static test_unit_t *root;
@@ -17,13 +17,11 @@ static int test_count;
 
 static void add_test(test_func_t func)
 {
-    test_unit_t *p1, *p2, *p3;
-
     test_unit_t *me = mz_malloc(sizeof(*me));
-    mz_list_init(&me->element);
+    mz_general_list_init(&me->element);
     me->test_func = func;
 
-    mz_list_add(&me->element, &root->element);
+    mz_general_list_add(&me->element, &root->element);
 }
 
 static void run_test()
@@ -31,7 +29,7 @@ static void run_test()
     test_unit_t *pos;
     test_count = 0;
 
-    mz_list_for_each_entry(pos, &root->element, test_unit_t, element) {
+    mz_general_list_for_each_entry(pos, &root->element, test_unit_t, element) {
         pos->test_func();
         test_count++;
     }
@@ -40,7 +38,7 @@ static void run_test()
 int main()
 {
     root = mz_malloc(sizeof(*root));
-    mz_list_init(&root->element);
+    mz_general_list_init(&root->element);
     
 
     logI("Start Unit Test");
