ce579ecdc9f636fc970bc6ee5e5e93059b0df274
patrick <patrick_dj@163.com>
2012-06-09 08:00:43 +0800

    add new yc_pkg.h.

--- yc/client/src/main.c
+++ yc/client/src/main.c
@@ -4,16 +4,32 @@
 #include <mz/mz_libs.h>
 #include <yc/package/yc_config.h>
 
+static void send_test_data(mz_rudp_t *rudp, mz_rudp_addr_t *dst)
+{
+    int i;
+    char buf[YC_BUFFER_SIZE];
+
+    logI("send test data.");
+
+    for (i = 0; i < 20; i++) {
+        mz_snprintf(buf, sizeof(buf), "test msg: %2d", i);
+        mz_rudp_send(rudp, buf, strlen(buf) + 1, dst);
+    }
+}
+
 int main(int argc, char **args)
 {
-    mz_rudp_t *me = mz_rudp_new(0);
+    mz_rudp_t *rudp = mz_rudp_new(0);
     mz_rudp_addr_t *dst = mz_rudp_addr_new("127.0.0.1", YC_SERVER_PORT);
 
     char buf[YC_BUFFER_SIZE];
 
-    assert(me->socket_fd > 0 && me->socket_fd < 0x8fff);
+    assert(rudp->socket_fd > 0 && rudp->socket_fd < 0x8fff);
 
     logI("message buffer size -> %d", YC_BUFFER_SIZE);
+
+    mz_time_sleep(1000);
+    send_test_data(rudp, dst);
 
     do {
         mz_memset(buf, 0, sizeof(buf));
@@ -21,10 +37,10 @@ int main(int argc, char **args)
         printf("Print message to send: ");
         scanf("%s", buf);
 
-        mz_rudp_send(me, buf, strlen(buf) + 1, dst);
+        mz_rudp_send(rudp, buf, strlen(buf) + 1, dst);
     } while (!mz_string_equal(buf, "client_quit"));
 
-    mz_rudp_delete(me);
+    mz_rudp_delete(rudp);
     mz_rudp_addr_delete(dst);
 
     mz_print_memory_log();
