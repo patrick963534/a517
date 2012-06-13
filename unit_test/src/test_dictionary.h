6fccf48e786987f367ca50342f6b5adb3da168ef
patrick <patrick_dj@163.com>
2012-06-13 17:42:30 +0800

    add unit test file for mz_dictionary type.

---
+++ unit_test/src/test_dictionary.h
@@ -0,0 +1,31 @@
+#ifndef __TEST_DICTIONARY_H__
+#define __TEST_DICTIONARY_H__
+
+#include <mz/mz_cunit.h>
+#include <mz/mz_libs.h>
+#include <mz/mz_dictionary.h>
+
+typedef struct item2_t
+{
+    int id;
+} item2_t;
+
+void test_dictionary()
+{
+    mz_dictionary_t *me = mz_dictionary_new();
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        char buf[8];
+        item2_t *it = mz_malloc(sizeof(*it));
+        it->id = i;
+        mz_snprintf(buf, sizeof(buf), "%d", i);
+        mz_dictionary_add(me, buf, it);
+    }
+
+    //mz_cunit_assert_int(MZ_TRUE, 5, me->count);
+
+    mz_dictionary_delete(me);
+}
+
+#endif
