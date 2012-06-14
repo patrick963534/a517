186449e19e9693e4951ded235b5c0a9e7e723eba
patrick <patrick_dj@163.com>
2012-06-14 18:00:24 +0800

    impelement mz_dictionary_contains...

--- lib/src/mz_dictionary.c
+++ lib/src/mz_dictionary.c
@@ -1,9 +1,10 @@
 #include <mz/mz_dictionary.h>
+#include <mz/mz_list.h>
 
 typedef struct mz_dictionary_item_t
 {
     char          *key;
-    const void    *value;
+    void    *value;
 } mz_dictionary_item_t;
 
 MZ_API mz_dictionary_t* mz_dictionary_new()
@@ -33,4 +34,18 @@ MZ_API void mz_dictionary_add(mz_dictionary_t *me, const char *key, void *value)
 MZ_API void mz_dictionary_clear(mz_dictionary_t *me)
 {
     mz_list_clear(me->root);
+}
+
+MZ_API void* mz_dictionary_contains(mz_dictionary_t *me, const char *key)
+{
+    mz_list_item_t *pos;
+    
+    mz_list_for_each_entry(pos, me->root, mz_list_item_t)
+    {
+        mz_dictionary_item_t *it = (mz_dictionary_item_t*)pos->ptr_ref;
+        if (mz_string_equal(it->key, key))
+            return it->value;
+    }
+
+    return NULL;
 }
