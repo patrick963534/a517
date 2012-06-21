5bf05cf313f5c21d80f3e78cea8e283578ab229c
patrick <patrick_dj@163.com>
2012-06-21 14:50:01 +0800

    [minibox]extract play sound.

--- minibox/src/main.c
+++ minibox/src/main.c
@@ -4,6 +4,7 @@
 #include <mz/mz_path.h>
 #include <mz/mz_list.h>
 #include <mz/mz_dictionary.h>
+#include "sound_play.h"
 
 enum list_type_t
 {
@@ -57,35 +58,6 @@ static GtkWidget* get_tree_view()
     return treeview;
 }
 
-static void play_sound_ogg_1(const char *file)
-{
-    char *uri;
-    GstElement *sink, *play;
-
-    sink = gst_element_factory_make("alsasink", "sink");
-    play = gst_element_factory_make("playbin2", "play");
-    uri = g_strdup_printf("file://%s", file);
-
-    g_object_set(G_OBJECT(play), "uri", uri, "audio-sink", sink, NULL);
-
-    if (!sink || !play || !uri) {
-        g_warning("One element could not be created!! (%s, %d)", __FILE__, __LINE__);
-        goto error;
-    }
-
-    g_print("start playing\n");
-    gst_element_set_state(play,GST_STATE_PLAYING);
-    mz_time_sleep(5000);
-    gst_element_set_state(play,GST_STATE_NULL);
-    g_print("stop playing\n");
-    
-error:
-    if (play) {
-        g_free(uri);
-        gst_object_unref(GST_OBJECT(play));
-    } 
-}
-
 static void view_row_activated (GtkTreeView        *treeview,
                          GtkTreePath        *path,
                          GtkTreeViewColumn  *col,
@@ -103,7 +75,7 @@ static void view_row_activated (GtkTreeView        *treeview,
 
         g_print("Double-clicked row contains string: %s\n", name);
 
-        play_sound_ogg_1((const char*)name);
+        play_sound_ogg((const char*)name);
 
         g_free(name);
     }
